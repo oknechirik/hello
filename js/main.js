3597bf6c8b7c1c8fa162e056d6180cc5c68dc431
sergey <oooott.serg@gmail.com>
2014-05-22 08:03:06 +0400

    fonts, js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-
+$(function(){
+    var mainSlider = $('.main-slider')
+        , workExamplesSlider = $('.work-examples-slider');
+    mainSlider.slick({
+        dots: true,
+//        infinite: true,
+//        slidesToShow: 1,
+//        slidesToScroll: 1,
+//        autoplay : true,
+        autoplaySpeed : 5000,
+        customPaging: function(slider, i) {
+            return '<b>' + (i + 1) + '</b>';
+        }
+    });
+    workExamplesSlider.slick({
+        slidesToShow: 3,
+        slidesToScroll: 3
+    })
+});
